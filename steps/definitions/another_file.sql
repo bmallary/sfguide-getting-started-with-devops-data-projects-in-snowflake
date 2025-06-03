68724266eb71734f431dc760f517f9804676ad74
bmallary <bmallary@pm.me>
2025-06-03 02:43:47 +0000

    Adding new definitions

---
+++ steps/definitions/another_file.sql
@@ -0,0 +1,7 @@
+--Gold
+{% set DB = 'GOLD'%}
+{% set SCHEMA = 'MEMBERSHIP'%}
+DEFINE TABLE {{DB}}_{{ENV}}_DB.{{SCHEMA}}.ANOTHER_TABLE
+(
+    DATA VARIANT
+);
---
+++ steps/definitions/yet_another_new file.sql
@@ -0,0 +1,7 @@
+--Gold
+{% set DB = 'GOLD'%}
+{% set SCHEMA = 'MEMBERSHIP'%}
+DEFINE TABLE {{DB}}_{{ENV}}_DB.{{SCHEMA}}.EVEN_MORE
+(
+    DATA INT
+);
